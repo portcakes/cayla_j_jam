2359b65e28c19dbd2f244d01f3ca441784a33ba4
Cayla Jefferson <cayla.jefferson4@gmail.com>
2025-06-12 02:43:10 -0400

    added random icon to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,43 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from './ui/button'
-import LogInForm from './Log-In-Form'
-import RegisterForm from './Register-Form'
+import { useUser, SignOutButton, SignUpButton, SignInButton } from '@clerk/nextjs'
+import { Laugh, SmilePlus, Smile, Angry, Annoyed, HandMetal, HeartHandshake, Heart,BicepsFlexed, ThumbsUp, Star, PartyPopper, Salad, LeafyGreen, PiggyBank, Gem, Ham, Sandwich, Croissant, Cake, Cherry, Coins, Gamepad, Ghost, Gift, Skull, Sparkles, Castle, Crown, Flower, Save, Bubbles, MoonStar, CableCar, CirclePower } from 'lucide-react'
+
 
 const Navbar = () => {
-    const [isLogIn, setIsLogIn] = useState(false)
-    const [isRegister, setIsRegister] = useState(false)
+    const { user } = useUser()
+    const icons = [Laugh, SmilePlus, Smile, Angry, Annoyed, HandMetal, HeartHandshake, Heart, BicepsFlexed, ThumbsUp, Star, PartyPopper, Salad, LeafyGreen, PiggyBank, Gem, Ham, Sandwich, Croissant, Cake, Cherry, Coins, Gamepad, Ghost, Gift, Skull, Sparkles, Castle, Crown, Flower, Save, Bubbles, MoonStar, CableCar, CirclePower]
+    const [Icon, setIcon] = useState(() => icons[0]) // Default to first icon
 
-  return (
-    <div className="flex justify-between items-center p-4">
-        <h1 className="text-2xl font-bold">Cayla's Website</h1>
-        <div className="flex gap-4">
-            {!isLogIn && !isRegister && (
-                <>
-                    <Button className="bg-blue-500 text-white p-6 rounded-md" onClick={() => setIsLogIn(true)}>Log In</Button>
-                    <Button className="bg-blue-500 text-white p-6 rounded-md" onClick={() => setIsRegister(true)}>Register</Button>
-                </>
-            )}
-            {isRegister && (
-                <>
-                    <Button className="bg-blue-500 text-white p-6 rounded-md" onClick={() => setIsRegister(false)}>Cancel</Button>
-                    <RegisterForm />
-                </>
-            )}
-            {isLogIn && (
-                <>
-                    <Button className="bg-blue-500 text-white p-6 rounded-md" onClick={() => setIsLogIn(false)}>Cancel</Button>
-                    <LogInForm />
-                </>
-            )}
+    useEffect(() => {
+        const randomIcon = icons[Math.floor(Math.random() * icons.length)]
+        setIcon(() => randomIcon)
+    }, []) // Empty dependency array means this runs once on mount
+
+    return (
+        <div className="flex justify-between items-center p-4">
+            <h1 className="text-2xl font-bold flex items-center gap-2"><Icon className="text-4xl" /> Cayla's Website</h1>
+            <div className="flex gap-4">
+                {!user && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button className="bg-blue-500 text-white p-6 rounded-md">Log In</Button>
+                        </SignInButton>
+                        <SignUpButton mode="modal">
+                            <Button className="bg-blue-500 text-white p-6 rounded-md">Register</Button>
+                        </SignUpButton>
+                    </>
+                )}
+
+                {user && (
+                    <SignOutButton>
+                        <Button className="bg-blue-500 text-white p-6 rounded-md">Log Out</Button>
+                    </SignOutButton>
+                )}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
